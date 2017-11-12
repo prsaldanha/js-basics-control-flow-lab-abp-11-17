0e6cb34c1f9d23ef6935df2c9b84454de80f7e8b
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:31:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride)
+{
+  if (ride <= 400)
+    return 'This one is on me!'
+  else if ride > 2000 && ride < 2500)
+    return 'I will gladly take your thirty bucks.'
+  else if ride >= 2500 {
+    return 'No can do.'
+
+}// Write your code in this file!
+
+function ternaryCheckCity(city)
+{
+  if (city === 'NYC')
+    return 'Ok, sounds good.'
+    else if (city === 'Pittsburgh')
+    return 'No go.'
+}
+
+function switchOnCharmFromTip(response)
+{
+  if (response === 'generous')
+  {
+    return 'Thank you so much.'
+  }
+  else if (response === 'not as generous')
+  {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye'
+  }
+}

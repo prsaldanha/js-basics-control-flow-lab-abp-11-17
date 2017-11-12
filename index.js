4fc5b3542d2552b9d9f06ab610e870cd57aec5b9
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function scuberGreetingForFeet(ride)
   {
     return "I will gladly take your thirty bucks."
   }
-  else if )ride >= 2500 {
+  else if (ride >= 2500 {
     return "No can do."
 
   }

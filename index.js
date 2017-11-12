9755948501ecac4c28a5dcae92fb264ebdf21d76
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ function scuberGreetingForFeet(ride)
 
 function ternaryCheckCity(city)
 {
-  if (city === 'NYC')
-    return 'Ok, sounds good.'
-    else if (city !== 'NYC')
-    return 'No go.'
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+     
 }
 
 function switchOnCharmFromTip(response)

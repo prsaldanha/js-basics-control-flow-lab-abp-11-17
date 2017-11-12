76d06fedb67d1860f8416447be0a28adbdb38dee
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:54:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride)
+{
+  if (ride <= 400)
+  {
+    return "This one is on me!"
+  }
+  else if (ride > 2000 && ride < 2500)
+  {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500) {
+    return "No can do."
+
+  }
+}
+
+function ternaryCheckCity(city)
+{
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+}
+
+function switchOnCharmFromTip(response)
+{
+  switch (response)
+  {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+    break;
+  }
+}

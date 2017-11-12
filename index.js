f373b3a5baa74c50448abe5dcaa16ce3372cbdb7
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:36:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function scuberGreetingForFeet (ride)
 {
   if (ride <= 400)
-    return 'This one is on me!'
+    console.log('This one is on me!');
   else if ride > 2000 && ride < 2500)
-    return 'I will gladly take your thirty bucks.'
+    console.log('I will gladly take your thirty bucks.');
   else if ride >= 2500 {
-    return 'No can do.'
+    console.log('No can do.');
 
 }// Write your code in this file!
 

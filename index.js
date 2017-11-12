fea767fe0b6cd5d05c596ef1bd50b8b7c89e41f9
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 05:53:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 function scuberGreetingForFeet(ride)
 {
-  if (ride <= 400)
-  {
-    return "This one is on me!"
-  }
-  else if (ride > 2000 && ride < 2500)
-  {
-    return "I will gladly take your thirty bucks."
-  }
-  else if (ride >= 2500 {
-    return "No can do."
 
-  }
 }
 
 function ternaryCheckCity(city)
